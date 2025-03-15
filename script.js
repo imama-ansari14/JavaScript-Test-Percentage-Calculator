f225864769c87b2440361c27fce7849db9403c29
Imama Ansari <ansariimama@yahoo.com>
2025-03-16 00:25:55 +0500

    Percentage and grade calculator is in process....

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// OBTAINED MARKS PROMPT HERE... 
+let mathsMarks = +prompt("Enter the Marks of your Maths Test");
+let physicsMarks = +prompt("Enter the Marks of your Physics Test");
+let compMarks = +prompt("Enter the Marks of your Computer Test");
+let urduMarks = +prompt("Enter the Marks of your Urdu Test");
+let engMarks = +prompt("Enter the Marks of your English Test");
+// ONE SUBJECT TOTAL MARKS HERE...
+let subTmarks=100;
+// TOTAL MARKS OF ALL SUBJECTS...
+let tMarks=500;
+// SUB TOTAL OF OBTAINED MARKS...
+let tObtMarks=mathsMarks+physicsMarks+compMarks+urduMarks+engMarks 
+// PERCENTAGE FUNCTION HERE....
+
+document.write("<table> <tr><th>Subject</th> <th>Total Marks</th> <th>Marks Obtained</th> <th>Grade</th> <th> Percentage</th> </tr> <tr><td>Maths</td> <td>"+ subTmarks+ "</td><td>"+mathsMarks+"</td><td> </td><td>"+userPer+"</td></tr> <tr><td> Physics</td> <td>"+subTmarks+" </td> <td>"+physicsMarks+"</td> <td> </td> <td> </td></tr>   <tr><td>Computer</td> <td>"+subTmarks+"</td> <td>"+compMarks+"</td> <td> </td> <td></td></tr> <tr><td>Urdu</td> <td>"+subTmarks+"</td> <td>"+urduMarks+"</td> <td> </td> <td> </td></tr>  <tr><td>English</td> <td>"+subTmarks+"</td><td>"+engMarks+"</td><td></td><td></td></tr>    <tr><td>SubTotal</td><td>"+tMarks+"</td><td>"+tObtMarks+"</td><td> </td><td> </td></tr> </table>")
